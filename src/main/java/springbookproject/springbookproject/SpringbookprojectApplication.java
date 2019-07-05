71d03e1a831b1ecb2990b2d74e954ed5e876007a
Fatma Degirmenci <fatmadegirmenci99@gmail.com>
2019-07-06 01:56:45 +0300

    proje olusturuldu

---
+++ src/main/java/springbookproject/springbookproject/SpringbookprojectApplication.java
@@ -0,0 +1,13 @@
+package springbookproject.springbookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbookprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbookprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springbookproject/springbookproject/SpringbookprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package springbookproject.springbookproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbookprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
