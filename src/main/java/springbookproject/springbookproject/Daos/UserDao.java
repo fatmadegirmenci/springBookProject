ff9f4211936b3bfd81ab06705001109dcaf93b27
Fatma Degirmenci <fatmadegirmenci99@gmail.com>
2019-07-08 09:51:50 +0300

    user dao guncellendi

--- src/main/java/springbookproject/springbookproject/Daos/UserDao.java
+++ src/main/java/springbookproject/springbookproject/Daos/UserDao.java
@@ -1,4 +1,35 @@
-package com.springbookproject.Daos;
+package springbookproject.springbookproject.Daos;
 
+import org.springframework.stereotype.Repository;
+import springbookproject.springbookproject.Beans.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public class UserDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void create(User user) {
+
+        try {
+            entityManager.persist(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(User user) {
+
+        try {
+            if(entityManager.contains(user))
+            entityManager.remove(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
